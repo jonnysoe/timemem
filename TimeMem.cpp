8b5deed31a0e0dc9b47d3df085b5d7f633d7bfa3
jonnysoe <84360198+jonnysoe@users.noreply.github.com>
2023-07-05 23:41:30 +0800

    Propagate error code and mirror time outputs
    - Propagate error code to calling process
    - Outputs to stderr
    - Reworded outputs to mirror time
    - Change console to unicode mode

--- TimeMem.cpp
+++ TimeMem.cpp
@@ -1,5 +1,7 @@
 // TimeMem.cpp - Windows port of Unix /usr/bin/time utility
 
+#include <fcntl.h>
+#include <io.h>
 #include <Windows.h>
 #include <Psapi.h>
 #include <processenv.h>
@@ -22,7 +24,7 @@ using auto_string = std::basic_string<_TCHAR>;
 namespace {
 // Displays usage help for this program.
 void usage() {
-    _tprintf(_T("Usage: TimeMem command [args...]\n"));
+    _ftprintf(stderr, _T("Usage: TimeMem command [args...]\n"));
 }
 
 // A makeshift where command that only returns the first instance of the command
@@ -36,7 +38,7 @@ std::vector<auto_string> where(_TCHAR* command) {
     SearchPath(nullptr, command, _T(".exe"), MAX_PATH, target[0].data(), nullptr);
 
     int err = GetLastError();
-    if (err == ENOENT) {
+    if ((err == ENOENT)) {
         // Reattempt with cmd.
         ///@todo: Need to account for PowerShell parent process.
         SearchPath(nullptr, _T("cmd.exe"), nullptr, MAX_PATH, target[0].data(), nullptr);
@@ -47,63 +49,84 @@ std::vector<auto_string> where(_TCHAR* command) {
 
 // Make the params pointer relative to full command line without needing to reconstruct arguments.
 // GetCommandLine() is typically the source for full command line.
-_TCHAR* partial_command(_TCHAR* const full_command, const std::vector<_TCHAR*>& args, int const offset) {
-    _TCHAR* params = _T("");
-    if (args.size() > offset) {
-        // Normally argv with spaces does not have quote `"` but full command line does,
-        // so -1 was blindly used to account for it.
-        params = _tcsstr(full_command, args[offset]) - 1;
-        if (*params != _T('\"')) {
-            // This argv does not have have space so `"` wasn't inserted, bump the starting pointer.
-            ++params;
-        }
-    }
-    return params;
+_TCHAR* strip_command(_TCHAR* const full_command) {
+    return _tcsstr(full_command, _T(" ")) + 1;
 }
 
 // Displays information about a process.
-int info(HANDLE hProcess) {
-    DWORD dwExitCode;
+DWORD info(HANDLE hProcess, _TCHAR* const command, _TCHAR* const full_command) {
+    // Get error code from CreateProcess
+    DWORD dwExitCode = GetLastError();
+    PROCESS_MEMORY_COUNTERS pmc = { sizeof(PROCESS_MEMORY_COUNTERS), 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    double tElapsed = 0;
+    double tKernel = 0;
+    double tUser = 0;
+    _ftprintf(stderr, _T("\n"));
 
-    // Exit code
-    if (!GetExitCodeProcess(hProcess, &dwExitCode)) {
-        return 1;
-    }
+    if (hProcess != nullptr) {
 
-    // CPU info
-    FILETIME64 ftCreation;
-    FILETIME64 ftExit;
-    FILETIME64 ftKernel;
-    FILETIME64 ftUser;
-    if (!GetProcessTimes(hProcess, &ftCreation.legacy, &ftExit.legacy, &ftKernel.legacy, &ftUser.legacy)) {
-        return 1;
-    }
-    double const tElapsed = 1.0e-7 * (ftExit.time - ftCreation.time);
-    double const tKernel = 1.0e-7 * ftKernel.time;
-    double const tUser = 1.0e-7 * ftUser.time;
+        // Exit code
+        switch (dwExitCode) {
+            ///@todo Generic prints, check GNU time behaviors
+            case ENOENT:
+                // CreateProcess failed due to file not found
+                _ftprintf(stderr, _T("TimeMem: cannot run %s: %s\n"), command, _tcserror(dwExitCode));
+                // GNU time command replace file not found with 127
+                dwExitCode = 127;
+                break;
+            case 0:
+                // Replace error code if CreateProcess was successful
+                GetExitCodeProcess(hProcess, &dwExitCode);
+                if (dwExitCode == 0) {
+                    break;
+                }
+            default:
+                // Take error code as is
+                _ftprintf(stderr, _T("%s: %s\n"), command, _tcserror(dwExitCode));
+                break;
+        }
 
-    // Memory info
-    // Print information about the memory usage of the process.
-    PROCESS_MEMORY_COUNTERS pmc = { sizeof(PROCESS_MEMORY_COUNTERS) };
-    if (!GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
-        return 1;
+        // CPU info
+        FILETIME64 ftCreation;
+        FILETIME64 ftExit;
+        FILETIME64 ftKernel;
+        FILETIME64 ftUser;
+        if (GetProcessTimes(hProcess, &ftCreation.legacy, &ftExit.legacy, &ftKernel.legacy, &ftUser.legacy)) {
+            tElapsed = 1.0e-7 * (ftExit.time - ftCreation.time);
+            tKernel = 1.0e-7 * ftKernel.time;
+            tUser = 1.0e-7 * ftUser.time;
+
+            // Memory info
+            // Print information about the memory usage of the process.
+            // More memory information?
+            // https://learn.microsoft.com/en-us/previous-versions/windows/desktop/legacy/aa965225(v=vs.85)
+            GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc));
+        }
     }
 
     // Display info.
-    _tprintf(_T("\n"));
-    _tprintf(_T("Exit code      : %u\n"), dwExitCode);
+    if (dwExitCode != 0) {
+        _ftprintf(stderr, _T("Command exited with non-zero status %u\n"), dwExitCode);
+    }
+    _ftprintf(stderr, _T("\tCommand being timed: %s\n"), full_command);
 
-    _tprintf(_T("Elapsed time   : %.2lf\n"), tElapsed);
-    _tprintf(_T("Kernel time    : %.2lf (%.1lf%%)\n"), tKernel, 100.0*tKernel/tElapsed);
-    _tprintf(_T("User time      : %.2lf (%.1lf%%)\n"), tUser, 100.0*tUser/tElapsed);
+    _ftprintf(stderr, _T("\tUser time (seconds): %.2lf\n"), tUser);
+    _ftprintf(stderr, _T("\tSystem time (seconds): %.2lf\n"), tKernel);
+    _ftprintf(stderr, _T("\tElapsed (wall clock) time (h:mm:ss or m:ss): %.2lf\n"), tElapsed);
 
-    _tprintf(_T("page fault #   : %u\n"), pmc.PageFaultCount);
-    _tprintf(_T("Working set    : %zd KB\n"), pmc.PeakWorkingSetSize/1024);
-    _tprintf(_T("Paged pool     : %zd KB\n"), pmc.QuotaPeakPagedPoolUsage/1024);
-    _tprintf(_T("Non-paged pool : %zd KB\n"), pmc.QuotaPeakNonPagedPoolUsage/1024);
-    _tprintf(_T("Page file size : %zd KB\n"), pmc.PeakPagefileUsage/1024);
+    // KIV temporary calculations which may not be accurate.
+    // Minor/Soft page faults, virtual memory swapped out to disk os needs to retrieve page from disk to physical memory
+    auto minor_page_fault = pmc.QuotaPeakPagedPoolUsage;
 
-    return 0;
+    _ftprintf(stderr, _T("\tMaximum resident set size (kbytes): %zd\n"), pmc.PeakWorkingSetSize/1024);
+    _ftprintf(stderr, _T("\tTotal page faults: %u\n"), pmc.PageFaultCount);
+    _ftprintf(stderr, _T("\tPaged pool     : %zd KB\n"), pmc.QuotaPeakPagedPoolUsage/1024);
+    _ftprintf(stderr, _T("\tNon-paged pool : %zd KB\n"), pmc.QuotaPeakNonPagedPoolUsage/1024);
+    _ftprintf(stderr, _T("\tPage file size : %zd KB\n"), pmc.PeakPagefileUsage/1024);
+
+    _ftprintf(stderr, _T("\tExit status: %u\n"), dwExitCode);
+
+    return dwExitCode;
 }
 }
 
@@ -117,6 +140,15 @@ int _tmain(int argc, _TCHAR* argv[]) {
     // argv[0] is always the current program so skip it.
     std::vector<_TCHAR*> args(argv + 1, argv + argc);
 
+#ifdef _UNICODE
+    if (!IsDebuggerPresent()) {
+        // Need to change terminal to unicode mode
+        // NOTE: vscode's debug console does not support unicode mode
+        _setmode(_fileno(stdout), _O_U16TEXT);
+        _setmode(_fileno(stderr), _O_U16TEXT);
+    }
+#endif
+
     // If we have no arguments, display usage info and exit.
     if (argc == 1)
     {
@@ -125,13 +157,13 @@ int _tmain(int argc, _TCHAR* argv[]) {
     }
 
     // Acquire a normalized target command.
-    auto command = where(args[0]);
+    auto command = where(argv[1]);
 
     // Get full command line.
-    _TCHAR* const full_command = partial_command(GetCommandLine(), args, 0);
+    _TCHAR* const full_command = strip_command(GetCommandLine());
 
     // Get first command argument.
-    _TCHAR* const params = partial_command(full_command, args, 1);
+    _TCHAR* const params = strip_command(full_command);
 
     // Append the arguments.
     command[1] += params;
@@ -141,19 +173,21 @@ int _tmain(int argc, _TCHAR* argv[]) {
     PROCESS_INFORMATION pi;
     if (!CreateProcess(command[0].data(), command[1].data(), nullptr, nullptr, false, 0, nullptr, nullptr, &si, &pi))
     {
-        _tprintf(_T("Error: Cannot create process.\n"));
-        return 1;
+        // Most likely won't ever get in anymore due to command replacement...
+        _ftprintf(stderr, _T("Error: Cannot create process.\n"));
+        return info(pi.hProcess, argv[1], full_command);
     }
 
     // Wait for the process to finish.
     if (WaitForSingleObject(pi.hProcess, INFINITE) != WAIT_OBJECT_0)
     {
-        _tprintf(_T("Error: Cannot wait for process.\n"));
-        return 1;
+        ///@todo This waits indefinitely, probably only useful for long processes to exit with Ctrl-C (SIGINT)
+        _ftprintf(stderr, _T("Error: Cannot wait for process.\n"));
+        return info(pi.hProcess, argv[1], full_command);
     }
 
     // Display process statistics.
-    int ret = info(pi.hProcess);
+    auto const ret = info(pi.hProcess, argv[1], full_command);
 
     // Close process handles.
     CloseHandle(pi.hThread);
